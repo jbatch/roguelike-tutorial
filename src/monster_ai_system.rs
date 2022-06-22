e1db74933c30a2cf10d38a50e517fd763228e41e
Josh Batchelor <josh_batch@live.com.au>
2022-06-22 19:53:10 +0800

    Adds monsters and monster AI system

---
+++ src/monster_ai_system.rs
@@ -0,0 +1,26 @@
+use crate::Name;
+
+use super::{Map, Monster, Position, Viewshed};
+use rltk::{console, field_of_view, Point};
+use specs::prelude::*;
+
+pub struct MonsterAI {}
+
+impl<'a> System<'a> for MonsterAI {
+    type SystemData = (
+        ReadExpect<'a, Point>,
+        ReadStorage<'a, Viewshed>,
+        ReadStorage<'a, Monster>,
+        ReadStorage<'a, Name>,
+    );
+
+    fn run(&mut self, data: Self::SystemData) {
+        let (player_pos, viewshed, monster, name) = data;
+
+        for (viewshed, _monster, name) in (&viewshed, &monster, &name).join() {
+            if viewshed.visible_tiles.contains(&*player_pos) {
+                console::log(&format!("{} shouts insult", name.name));
+            }
+        }
+    }
+}
--- src/player.rs
+++ src/player.rs
@@ -1,8 +1,10 @@
+use rltk::Point;
 use rltk::{Rltk, VirtualKeyCode};
 use specs::prelude::*;
 use specs::World;
 use std::cmp::{max, min};
 
+use crate::RunState;
 use crate::Viewshed;
 
 use super::{Map, Player, Position, State, TileType};
@@ -20,14 +22,17 @@ pub fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
             pos.y = min(79, max(0, pos.y + delta_y));
 
             viewshed.dirty = true;
+            let mut ppos = ecs.write_resource::<Point>();
+            ppos.x = pos.x;
+            ppos.y = pos.y;
         }
     }
 }
 
-pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
+pub fn player_input(gs: &mut State, ctx: &mut Rltk) -> RunState {
     // Player movement
     match ctx.key {
-        None => {}
+        None => return RunState::Paused,
         Some(key) => match key {
             VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
                 try_move_player(-1, 0, &mut gs.ecs)
@@ -44,7 +49,8 @@ pub fn player_input(gs: &mut State, ctx: &mut Rltk) {
             VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
                 try_move_player(0, 1, &mut gs.ecs)
             }
-            _ => {}
+            _ => return RunState::Paused,
         },
     }
+    RunState::Running
 }
